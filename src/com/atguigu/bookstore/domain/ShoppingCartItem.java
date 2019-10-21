4644db30b61858775baaceb206ad9f4a0e6b44bd
tom_plb <goodMorning_glb@atguigu.com>
2019-10-21 21:58:57 +0800

    8. 尚硅谷_佟刚_JavaWEB案例_加入购物车之新建ShoppingCartItem类

--- src/com/atguigu/bookstore/domain/ShoppingCartItem.java
+++ src/com/atguigu/bookstore/domain/ShoppingCartItem.java
@@ -2,15 +2,17 @@ package com.atguigu.bookstore.domain;
 
 /**
  *2019年1月7日上午10:30:17
- *
+ *2019年10月21日21:35:10
+ *封装了购物车中的商品，以及对书的引用和数量
  */
+
 public class ShoppingCartItem {
 
 	private Book book;
 	private int quantity;
 	public ShoppingCartItem(Book book) {
 		this.book = book;
-		this.quantity = 1;
+		this.quantity = 1;//初始化书的数量，就是刚添加进购物车的时候书的数量是1.
 	}
 	
 	public Book getBook() {
